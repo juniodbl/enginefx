db7f461791f4f0bca923bbd88faf1ee53d11ae3e
juniodbl <juniodbl@hotmail.com>
2021-04-05 21:59:41 -0300

    add adjusts

--- src/br/com/engine/componentes/audio/AudioEffect.java
+++ src/br/com/engine/componentes/audio/AudioEffect.java
@@ -25,6 +25,15 @@ public class AudioEffect extends SimpleComponent
 		clip.play( );
 	}
 	
+	/**
+	 * volume The relative volume level at which the clip is played. Valid range is 0.0 (muted) to 1.0 (full volume). Values are clamped to this range internally so values outside this range will have no additional effect. Volume is controlled by attenuation, so values below 1.0 will reduce the sound level accordingly.
+	 * @param volume
+	 */
+	public void setVolume( double volume )
+	{
+		this.clip.setVolume( volume );
+	}
+
 	public void stop()
 	{
 		clip.stop( );
